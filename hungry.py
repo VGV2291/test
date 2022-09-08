bca6340ea654f1c1746769450dcd2cc4fcf9be2a
rohit.m.dataengineer@gmail.com <rohit.m.dataengineer@gmail.com>
2022-09-08 09:18:06 -0500

    thirsty branch v1

--- hungry.py
+++ hungry.py
@@ -3,4 +3,6 @@ if hungry=="yes":
     print("eat samosa")
     print("eat pizza")
 else:
-    print("dont eat")
+    thirsty = input("are you thirsty")
+    if thirsty == "yes":
+        print("drink water")
